52c16fd0ef5a4440c716700769d8232d4130b775
daizxn <daizxn@outlook.com>
2025-03-02 15:17:09 +0800

    添加纯文本分析

--- api.py
+++ api.py
@@ -1,11 +1,10 @@
 from types import SimpleNamespace
 
+from openai import OpenAI
 from transformers import BertTokenizerFast
-
 
 from models.HAMMER import HAMMER
 import torch
-
 
 from tools.utils import load_and_preprocess_image
 
@@ -31,7 +30,7 @@ class FakeNewsDetector(AbstractFakeNewsDetector):
             init_deit=True)
         checkpoint_dir = args.hammer_checkpoint
         checkpoint = torch.load(checkpoint_dir, map_location=self.device)
-        self.all_multicls_dict = {0:'face_swap',1:'face_attribute',2:'text_swap',3:'text_attribute'}
+        self.all_multicls_dict = {0: 'face_swap', 1: 'face_attribute', 2: 'text_swap', 3: 'text_attribute'}
         # model.load_state_dict(state_dict)
 
         self.model.load_state_dict(checkpoint['model'], strict=False)
@@ -40,11 +39,12 @@ class FakeNewsDetector(AbstractFakeNewsDetector):
 
     def predict(self, text, image_path=None, mode='text'):
 
-        if mode=='multi-label':
-            text_input = self.tokenizer(text, padding='max_length', max_length=128, truncation=True, return_tensors='pt')
+        if mode == 'multi-label':
+            text_input = self.tokenizer(text, padding='max_length', max_length=128, truncation=True,
+                                        return_tensors='pt')
             text_input = {k: v.to(self.device) for k, v in text_input.items()}
             text_input = SimpleNamespace(**text_input)
-            image,ori_width,ori_height = load_and_preprocess_image(image_path)
+            image, ori_width, ori_height = load_and_preprocess_image(image_path)
             image = image.to(self.device)
             with torch.no_grad():
                 logits_real_fake, logits_multicls, output_coord, logits_tok, attn_weights = self.model(image=image,
@@ -59,15 +59,15 @@ class FakeNewsDetector(AbstractFakeNewsDetector):
                 cls_pred = logits_multicls[:, cls_idx]
                 cls_pred[cls_pred >= 0] = 1
                 cls_pred[cls_pred < 0] = 0
-                if cls_pred==1:
+                if cls_pred == 1:
                     pred_all_multicls.append(self.all_multicls_dict[cls_idx])
 
             x_c, y_c, w, h = output_coord.unbind(-1)
 
-            ori_x=x_c.cpu().numpy().item() * ori_width
-            ori_y=y_c.cpu().numpy().item() * ori_height
-            ori_w=w.cpu().numpy().item() * ori_width
-            ori_h=h.cpu().numpy().item() * ori_height
+            ori_x = x_c.cpu().numpy().item() * ori_width
+            ori_y = y_c.cpu().numpy().item() * ori_height
+            ori_w = w.cpu().numpy().item() * ori_width
+            ori_h = h.cpu().numpy().item() * ori_height
 
             box = [ori_x, ori_y, ori_w, ori_h]
 
@@ -80,16 +80,17 @@ class FakeNewsDetector(AbstractFakeNewsDetector):
 
             word_preds = []
             for i, token in enumerate(self.tokenizer.convert_ids_to_tokens(text_input.input_ids[0])):
-                if i==0:
+                if i == 0:
                     continue
-                if i>=valid_length:
+                if i >= valid_length:
                     break
                 word_preds.append({'token': token, 'pred': int(tok_pred[i])})
 
             return pred_cls, pred_all_multicls, box, word_preds
 
-        elif mode=='text':
-            text_input = self.tokenizer(text, padding='max_length', max_length=128, truncation=True, return_tensors='pt')
+        elif mode == 'text':
+            text_input = self.tokenizer(text, padding='max_length', max_length=128, truncation=True,
+                                        return_tensors='pt')
             text_input = {k: v.to(self.device) for k, v in text_input.items()}
             text_input = SimpleNamespace(**text_input)
             with torch.no_grad():
@@ -104,10 +105,39 @@ class FakeNewsDetector(AbstractFakeNewsDetector):
             valid_length = text_input.attention_mask[0].sum().item() - 1  # 减去CLS token
             word_preds = []
             for i, token in enumerate(self.tokenizer.convert_ids_to_tokens(text_input.input_ids[0])):
-                if i==0:
+                if i == 0:
                     continue
-                if i>=valid_length:
+                if i >= valid_length:
                     break
                 word_preds.append({'token': token, 'pred': int(tok_pred[i])})
 
-            return logits_real_fake ,{},{}, word_preds
+            return logits_real_fake, {}, {}, word_preds
+
+
+class AbstractLLMModel:
+    def __init__(self, url, api_key):
+        self.url = url
+        self.api_key = api_key
+
+    def chat(self, data):
+        raise NotImplementedError
+
+
+class LLModel(AbstractLLMModel):
+    def __init__(self, url, api_key, system_content):
+        super().__init__(url, api_key)
+        self.system_content = system_content
+        self.client = OpenAI(
+            api_key=self.api_key,
+            base_url=self.url
+        )
+
+    def chat(self, data):
+        completion = self.client.chat.completions.create(
+            model="ep-20250226134125-8h4b5",  # your model endpoint ID
+            messages=[
+                {"role": "system", "content": self.system_content},
+                {"role": "user", "content":data}
+            ]
+        )
+        return completion.choices[0].message.content
--- server.py
+++ server.py
@@ -3,17 +3,32 @@ import argparse
 import connexion
 import os
 import yaml
-from flask import send_from_directory, redirect, jsonify, request
+from flask import jsonify, request
 
-
-from api import FakeNewsDetector
+from api import FakeNewsDetector, LLModel
 
 UPLOAD_FOLDER = './image'  # 上传目录
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-
-
 lm = None
+
+deepseekLm = None
+
+content = "现在你要求扮演一位文本助手，要根据以下内容去编写说明文档。先你要扮演一位新闻专家，你要求根据以下材料说明该新闻为何为假或，特别的，我们提供的理由必然准确，同时个别新闻会有图片，我们会提供图片的造假情况，请你结合存在的图片造假情况和文本情况，去说明该新闻为假的理由，请注意，我们提供的材料必然为真实材料，不要质疑材料准确性，假新闻类型以空格为分割，新闻造假类型以假新闻类型为准，若不存在假新闻类型则为文本造假，可能存在的假词以空格为分割，每个词为独立的词，可能存在的假词仅为该token可能是否造假，请结合文本判断，图片可能无法提供，请你以图片存在的情况下，对文本和材料进行分析"
+
+
+def process_data(data,news_content):
+    news_true_or_false = "真" if data["pred_cls"] != 0 else "假"
+    news_type = ""
+    for type in data["pred_all_multicls"]:
+        news_type += type + " "
+    news_fake_token = ""
+    for word in data["word_preds"]:
+        if word["pred"] == 1:
+            news_fake_token += word["token"] + " "
+    message = f"这是一条{news_true_or_false}新闻，新闻造假类型是{news_type}，新闻内容是{news_content}，新闻中的假词有{news_fake_token}"
+    return message
+
 
 def upload():
     # 获取上传的文件
@@ -25,23 +40,34 @@ def upload():
         file_path = os.path.join(UPLOAD_FOLDER, file.filename)
         file.save(file_path)
 
-        return jsonify(message="Image uploaded successfully",image_path=file_path)
+        return jsonify(message="Image uploaded successfully", image_path=file_path)
     else:
         return jsonify(message="No file uploaded"), 400
 
-def analyze(analyze_request):
-    text=analyze_request.get('text')
-    image_path=analyze_request.get('image')
-    mode=analyze_request.get('mode')
-    pred_cls, pred_all_multicls, box, word_preds= lm.predict(text,image_path,mode)
+
+def predict(predict_request):
+    text = predict_request.get('text')
+    image_path = predict_request.get('image')
+    mode = predict_request.get('mode')
+    pred_cls, pred_all_multicls, box, word_preds = lm.predict(text, image_path, mode)
 
     res = {
         'pred_cls': int(pred_cls),
-        'pred_all_multicls':  pred_all_multicls,
+        'pred_all_multicls': pred_all_multicls,
         'box': box,
         'word_preds': word_preds
     }
 
+    return jsonify(res)
+
+
+def llm_analyze(analyse_request):
+    data = analyse_request.get('data')
+    news_content=analyse_request.get('text')
+    analyse = deepseekLm.chat(process_data(data,news_content))
+    res = {
+        'analyse': analyse
+    }
     return jsonify(res)
 
 
@@ -72,20 +98,22 @@ parser.add_argument('--token_momentum', default=False, action='store_true')
 parser.add_argument('--test_epoch', default='best', type=str)
 parser.add_argument('--log', action='store_true')
 
-#服务器相关
+# 服务器相关
 parser.add_argument("--nodebug", default=True)
 parser.add_argument("--address",
                     default="127.0.0.1")  # 0.0.0.0 for nonlocal use
 parser.add_argument("--port", default="5001")
 parser.add_argument("--nocache", default=False)
 
-
 parser.add_argument("--no_cors", action='store_true')
 if __name__ == '__main__':
     args = parser.parse_args()
-    app.run(port=int(args.port),  host=args.address)
+    app.run(port=int(args.port), host=args.address)
 else:
     args, _ = parser.parse_known_args()
     config = yaml.load(open(args.config, 'r'), Loader=yaml.Loader)
     # load_projects(args.dir)
-    lm = FakeNewsDetector(args=args,config=config)
+    lm = FakeNewsDetector(args=args, config=config)
+    deepseekLm = LLModel(url="https://ark.cn-beijing.volces.com/api/v3",
+                         api_key='05fe7d3b-aa40-4a54-b080-ac06c5a371b5',
+                         system_content=content)
